9370f70f7236b8768b4060d979c1b30a682bd812
Hoang Nguyen <38454021+hoangdesu@users.noreply.github.com>
2022-09-06 04:22:35 +0700

    Initial Commit

---
+++ PhimMoi-iOS/PhimMoi_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhimMoi_iOSApp.swift
+//  PhimMoi-iOS
+//
+//  Created by ドロケ on 06/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct PhimMoi_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
