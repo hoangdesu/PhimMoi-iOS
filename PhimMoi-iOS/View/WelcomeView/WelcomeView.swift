4eedcb36b4ea3fe85cf07589bce61a3e552f0d0f
Uyen <uyennmd>
2022-09-16 20:16:37 +0700

    WelcomeView Feature card and search view

--- PhimMoi-iOS/View/WelcomeView/WelcomeView.swift
+++ PhimMoi-iOS/View/WelcomeView/WelcomeView.swift
@@ -13,75 +13,48 @@ import YouTubePlayerKit
 let colorNavAppear = UINavigationBarAppearance()
 
 struct WelcomeView: View {
-//    @State var movie = [Movie]
-    @State var show = false
-    @State var  player : YouTubePlayer = ""
-//    @EnvironmentObject var movieVM: MovieViewModel
-
-    
-    init() {
-        colorNavAppear.configureWithOpaqueBackground()
-        colorNavAppear.backgroundColor = UIColor.white
-//        colorNavAppear.titleTextAttributes = [.font: UIFont.systemFont(ofSize: 15)]
-//        colorNavAppear.largeTitleTextAttributes = [.font: UIFont.systemFont(ofSize: 15)]
-
-
-        UIScrollView.appearance().backgroundColor = UIColor.white
-        UINavigationBar.appearance().standardAppearance =  colorNavAppear
-        UINavigationBar.appearance().scrollEdgeAppearance =  colorNavAppear
-//        // hide the form background
-//        UITableView.appearance().backgroundColor = .clear
-//        UINavigationBarAppearance().configureWithTransparentBackground()
-//        UINavigationBarAppearance().backgroundColor = .none
-//        // hide the background colour of the top toolbar
-//        UINavigationBar.appearance().barTintColor = .clear
-
-    }
+    @State private var search = false
     var body: some View {
-//        NavigationView {
-//            List(movie, id: \.id) { movie in
-//                Group {
-//                    Text("ID: \(movie.id)")
-//                    Text("Title: \(movie.title)")
-////                    if let overview = movie.overview { // bug: unwrap to no value
-////                        Text("Overview: \(overview)")
-////                    }
-//                    Button {
-//                        show.toggle()
-//
-//                    } label: {
-//                        Text("Test")
-//                    }
-//                    .sheet(isPresented: $show) {
-//                        Test(movie: movie)
-//                    }
-//                }
-//            }
-//            .navigationTitle("")
-//                        .navigationBarTitleDisplayMode(.inline)
-//                        .toolbar { // <2>
-//                                    ToolbarItem(placement: .principal) { // <3>
-//                                        Text("PhimMoi").font(.headline)
-//                                    }
-//                                }
-//        }
+        ZStack {
+            Color.white
+                .edgesIgnoringSafeArea(.all)
+            NavigationView {
+                ZStack {
+                    ScrollView {
+                        VStack {
+                            NavigationLink(destination: SearchView(),
+                                                           isActive: self.$search)
+                                            { EmptyView() }
+                                                .frame(width: 0, height: 0)
+                                                .disabled(true)
+                            ScrollView(.horizontal,
+                                       showsIndicators: false) {
+                                HStack(spacing: 20) {
+                                    FeatureCard()
+                                    FeatureCard()
+                                    FeatureCard()
+                                }.padding(20)
+                            }
+                        }
+                    }.navigationTitle("")
+                    .navigationBarTitleDisplayMode(.inline)
+                    .toolbar {
+                        ToolbarItem(placement: .principal){
+                            HStack {
+                                Spacer()
+                                Text("PhimMoi")
+                                    .bold()
+                                Spacer()
+                                Button(action:{ self.search = true }) { Image("search") }
+                            }
+                        }
+                    }
+                }
+                
+                
+            }
+        }
         
-        
-//        NavigationView {
-//            NavigationLink{
-//                Test()
-//            } label: {
-//                Text("Test")
-//                    .navigationBarTitleDisplayMode(.inline)
-//            }.navigationTitle("")
-//            .navigationBarTitleDisplayMode(.inline)
-//            .toolbar { // <2>
-//                        ToolbarItem(placement: .principal) { // <3>
-//                            Text("PhimMoi").font(.headline)
-//                        }
-//                    }
-//        }
-        Text("")
     }
 }
 
