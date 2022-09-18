11b30c616057bbe4f82801185a9a62533577ff8e
Uyen <uyennmd>
2022-09-18 18:35:59 +0700

    animation and signup view

--- PhimMoi-iOS/View/LoginView/SignupView.swift
+++ PhimMoi-iOS/View/LoginView/SignupView.swift
@@ -8,8 +8,57 @@
 import SwiftUI
 
 struct SignupView: View {
+    @State var name = ""
+    @State var email = ""
+    @State var password = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Image("login")
+                .resizable()
+                .ignoresSafeArea()
+            VStack(spacing: 20) {
+                Spacer()
+                Image("logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 250)
+                    .padding(.bottom, 50)
+                
+                TextField("Name", text: $name)
+                    .disableAutocorrection(true)
+                    .font(.system(size: 25, weight: .regular, design: .rounded))
+                    .textFieldStyle(.roundedBorder)
+                    .padding([.leading, .trailing], 70)
+                
+                TextField("Email", text: $email)
+                    .disableAutocorrection(true)
+                    .font(.system(size: 25, weight: .regular, design: .rounded))
+                    .textFieldStyle(.roundedBorder)
+                    .textInputAutocapitalization(.never)
+                    .padding([.leading, .trailing], 70)
+                
+                TextField("Password", text: $password)
+                    .disableAutocorrection(true)
+                    .textFieldStyle(.roundedBorder)
+                    .font(.system(size: 25, weight: .regular, design: .rounded))
+                    .textInputAutocapitalization(.never)
+                    .padding([.leading, .trailing], 70)
+                
+                Button {
+                    print("signup")
+                }
+                label: {
+                    Text("Sign Up")
+                        .foregroundColor(.white)
+                        .padding()
+                        .padding([.leading, .trailing], 98)
+                        .background(.red)
+                        .cornerRadius(10)
+                }
+                Spacer()
+            }
+        }
     }
 }
 
